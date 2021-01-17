704836da90073fedbde1ee1c063c6e7e7e588871
Kataevskii Mikhail <kataevskiymikhail@gmail.com>
2021-01-17 01:28:44 +0100

    v0.03

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
+const int width = 1600, height = 900;
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *mainWindow = SDL_CreateWindow("Map_Coloring", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1200, 600, SDL_WINDOW_SHOWN);
+    SDL_Window *mainWindow = SDL_CreateWindow("Map_Coloring", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
     SDL_Renderer *mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
     SDL_RenderClear(mainRenderer);
@@ -32,6 +34,27 @@ int main(int argc, char *argv[])
             {
                 if (!(SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)))
                     drawing = false;
+            }
+            else if (e.type == SDL_KEYDOWN)
+            {
+                if (e.key.keysym.sym == SDLK_RETURN)
+                {
+                    SDL_Surface *tempSurface = SDL_CreateRGBSurface(0, width, height, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+                    SDL_RenderReadPixels(mainRenderer, NULL, SDL_PIXELFORMAT_RGBA32, tempSurface->pixels, tempSurface->pitch);
+                    imageRGBA map(width, height);
+                    for (int i = 0; i < height; i++)
+                        for (int j = 0; j < width; j++)
+                        {
+                            pixelRGBA pixel;
+                            pixel.r = (Uint8)(((Uint32 *)tempSurface->pixels)[i * width + j] >> 16);
+                            pixel.g = (Uint8)(((Uint32 *)tempSurface->pixels)[i * width + j] >> 8);
+                            pixel.b = (Uint8)(((Uint32 *)tempSurface->pixels)[i * width + j] >> 0);
+                            pixel.a = (Uint8)(((Uint32 *)tempSurface->pixels)[i * width + j] >> 24);
+
+                            map.setPixel(j, i, pixel);
+                        }
+                    saveImageRGBA("test.png", map);
+                }
             }
         }
 
